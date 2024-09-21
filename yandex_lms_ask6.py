5dc3fddd44fc4531d4ec6ca7296b8e16a805aaa9
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-09-21 17:04:37 +0300

    completely rewrote the code to a more practical one, task completed

--- yandex_lms_ask6.py
+++ yandex_lms_ask6.py
@@ -1,25 +1,24 @@
+from collections import Counter
+import sys
+# Чтение входных данных
+initial_word = input().strip()
+words = [sys.stdin.readlines()]
+print(words[0])
+words = [words[0][i].strip() for i in range(len(words[0]))]
 
+# Подсчет частоты букв в исходном слове
+initial_counter = Counter(initial_word)
 
-def main():
-    user_input = [input() for x in range(8)]
-    user_input = [x.strip() for x in user_input]
-    game_word = user_input[0]
-    user_input = user_input[1:]
-    aplha = {}
-    true_words = []
-    for x in game_word:
-        aplha[x]: game_word.count(x)
-    for x in user_input:
-        lenght = len(x)
-        count = 0
-        for symbol in x:
-            if symbol in aplha:
-                if x.count(symbol) <= aplha[symbol]:
-                    count += 1
-        if count == lenght:
-            true_words.append(x)
-    print(true_words)
+# Список для хранения корректных слов
+valid_words = []
 
+# Проверка каждого слова
+for word in words:
+    word_counter = Counter(word)
+    if all(word_counter[char] <= initial_counter[char] for char in word_counter):
+        valid_words.append(word)
 
-if __name__ == '__main__':
-    main()
+# Вывод результатов
+print(len(valid_words))
+for valid_word in valid_words:
+    print(valid_word)
