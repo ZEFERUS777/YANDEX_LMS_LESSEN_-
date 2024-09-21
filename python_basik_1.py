19e4fe9ee8a229de249e3a54628aefb708fe648d
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-09-21 09:27:06 +0300

    Completed the code for the second Yandex task

---
+++ python_basik_1.py
@@ -0,0 +1,15 @@
+data = tuple((input().split(maxsplit=1)) for _ in range(int(input())))
+one_axis = tuple([(x, y) for x, y in data if x == 0 or y == 0])
+
+first_quarter, second_quarter, third_quarter, fourth_quarter = [], [], [], []
+for cord in data:
+    if int(cord[0]) > 0 and int(cord[1]) > 0:
+        first_quarter.append(cord)
+    elif int(cord[0]) < 0 and int(cord[1]) > 0:
+        second_quarter.append(cord)
+    elif int(cord[0]) < 0 and int(cord[1]) < 0:
+        third_quarter.append(cord)
+    elif int(cord[0]) > 0 and int(cord[1]) < 0:
+        fourth_quarter.append(cord)
+print(one_axis)
+print(f'I: {len(first_quarter)}, II: {len(second_quarter)}, III: {len(third_quarter)}, IV: {len(fourth_quarter)}')
