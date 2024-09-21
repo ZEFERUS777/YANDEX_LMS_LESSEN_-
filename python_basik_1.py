b9a699d0acd4f1d82bf93164738f34f44775aedf
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-09-21 09:53:57 +0300

    I removed bugs and improved the code. task completedI removed bugs and improved the code. task completed

--- python_basik_1.py
+++ python_basik_1.py
@@ -1,5 +1,5 @@
 data = tuple((input().split(maxsplit=1)) for _ in range(int(input())))
-one_axis = tuple([(x, y) for x, y in data if x == 0 or y == 0])
+one_axis = list((int(x), int(y)) for x, y in data if int(x) == 0 or int(y) == 0)
 
 first_quarter, second_quarter, third_quarter, fourth_quarter = [], [], [], []
 for cord in data:
@@ -11,5 +11,6 @@ for cord in data:
         third_quarter.append(cord)
     elif int(cord[0]) > 0 and int(cord[1]) < 0:
         fourth_quarter.append(cord)
-print(one_axis)
-print(f'I: {len(first_quarter)}, II: {len(second_quarter)}, III: {len(third_quarter)}, IV: {len(fourth_quarter)}')
+for x in one_axis:
+    print(x)
+print(f'I: {len(first_quarter)}, II: {len(second_quarter)}, III: {len(third_quarter)}, IV: {len(fourth_quarter)}.')
