c8a99f4a8aa9afe2fe2b35ced410031c60626583
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-09-22 14:22:12 +0300

    I made two more classes, problem: the last class with octave does not work correctly

--- yandex_lessen_2_3.py
+++ yandex_lessen_2_3.py
@@ -2,12 +2,14 @@ PITCHES = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
 
 
 class Note:
-    def __init__(self, pitch_note, duration=False):
+    def __init__(self, pitch_note, duration=False, octave=None):
         self.pitch_note = pitch_note
-        self.duration = duration  # True if duration is not None
+        self.octave = octave
+        self.is_long = duration
+        self.octave = octave
 
     def __str__(self):
-        if self.duration:  # Можно сократить условие
+        if self.is_long:  # Можно сократить условие
             if self.pitch_note == 'до':
                 return f'{self.pitch_note}-о'
             elif self.pitch_note == 'ре':
@@ -24,20 +26,63 @@ class Note:
                 return f'{self.pitch_note[:1]}-оль'
             else:
                 return f'{self.pitch_note}-{self.pitch_note[-1]}'
+        elif self.octave != None and self.is_long:
+            if self.pitch_note == 'до':
+                return f'{self.pitch_note}-о ({self.octave})'
+            elif self.pitch_note == 'ре':
+                return f'{self.pitch_note}-э {self.octave}'
+            elif self.pitch_note == 'ми':
+                return f'{self.pitch_note}-и ({self.octave})'
+            elif self.pitch_note == 'фа':
+                return f'{self.pitch_note}-а ({self.octave})'
+            elif self.pitch_note == 'ля':
+                return f'{self.pitch_note}-а ({self.octave})'
+            elif self.pitch_note == 'си':
+                return f'{self.pitch_note}-и ({self.ocatve})'
+            elif self.pitch_note == 'соль':
+                return f'{self.pitch_note[:1]}-оль ({self.octave})'
+            else:
+                return f'{self.pitch_note}-{self.pitch_note[-1]}'
         else:
             return self.pitch_note
 
 
 class LoudNote(Note):
-    def __init__(self, pitch_note):
+    def __init__(self, pitch_note, is_long=False):
         self.pitch_note = pitch_note
+        self.is_long = is_long
+        super().__str__
 
     def __str__(self):
         return self.pitch_note.upper()
 
 
 class DefaultNote(Note):
-    def __init__(self, pitch_note='до', duration=False):
+    def __init__(self, pitch_note='до', is_long=False):
+        self.octave = None
         self.pitch_note = pitch_note
-        self.duration = duration
+        self.is_long = is_long
+        super().__str__
+
+
+class NoteWithOctave(Note):
+    def __init__(self, pitch_note, octave, is_long=False):
+        self.pitch_note = pitch_note
+        self.is_long = is_long
+        self.octave = octave
         super().__str__()
+
+
+print(Note("соль"))
+
+print(LoudNote(PITCHES[4]))
+print(LoudNote("си", is_long=True))
+
+print(DefaultNote("ми"))
+print(DefaultNote())
+print(DefaultNote(is_long=True))
+
+print(NoteWithOctave("ре", "первая октава"))
+print(NoteWithOctave("ля", "малая октава", is_long=True))
+
+
