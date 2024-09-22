5adf623153222685585aa087a99c3781db8fb560
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-09-22 17:42:38 +0300

    done the class, assignment passed

--- yandex_ask_2_3.py
+++ yandex_ask_2_3.py
@@ -1,7 +1,9 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, name, x, y):
+        self.name = name
         self.x = x
         self.y = y
+        self.cord = tuple((x, y))
 
     def get_x(self):
         return self.x
@@ -9,6 +11,14 @@ class Point:
     def get_y(self):
         return self.y
 
+    def __str__(self):
+        return f'{self.name}{tuple((self.x, self.y))}'
+
+    def invert(self):
+        return Point(self.name, self.y, self.x)
+
+    def __invert__(self):
+        return self.invert()
+
     def get_coords(self):
         return tuple((self.x, self.y))
-
